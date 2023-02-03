defd4bffa1cc016a02497697e2ce14cee5e6a19b
Raghunath <rg000149@outlook.com>
2023-02-03 13:59:39 +0530

    Code for lop testing

---
+++ Numbers.java
@@ -0,0 +1,12 @@
+public class  Numbers
+{
+	public static void main(String args[])
+	{
+                int i;
+		for (i=1; i <=10; i++){
+			System.out.println("DevOps: " +i);
+	}
+    }
+}
+
+    
